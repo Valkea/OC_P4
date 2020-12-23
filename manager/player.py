e3bb499c994bb21bf5e55826ddd90c99cdaf60aa
Valkea <github-sub@shedge.com>
2020-12-23 01:41:58 +0100

    fix(player): fix birthdate format
    fix(player): fix __repr__ format

--- manager/player.py
+++ manager/player.py
@@ -1,25 +1,46 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" The purpose of this module is to handle the players of the tournaments
+""" The purpose of this module is to handle the actors of the tournaments
 """
 
 import json
 
-
 class Player:
+    """The purpose of this class is handle the chess players
+
+    Attributes
+    ----------
+    first_name : str
+    family_name : str
+    birthdate : str
+        must be a 'DD/MM/YYYY' string 
+    sex = str
+        can be either 'M' or 'F'
+    elo = int
+        must be greater than 0
+
+    Methods
+    -------
+    toJSON()
+        convert the current instance to a JSON dictionnary
+    """
+
     def __init__(self, family_name, first_name, birthdate, sex, elo):
-        self.family_name = family_name
-        self.first_name = first_name
-        self.birthdate = birthdate
-        self.sex = sex
-        self.elo = elo
+        try:
+            self.family_name = family_name
+            self.first_name = first_name
+            self.sex = sex
+            self.elo = elo
+            self.birthdate = birthdate 
+        except Exception as e:
+            print(e)
 
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
     def __repr__(self):
         return (
-            f"Player({self.family_name}, {self.first_name}, "
-            f"{self.birthdate}, {self.sex}, {self.elo})"
+            f"Player('{self.family_name}', '{self.first_name}', "
+            f"'{self.birthdate}', '{self.sex}', {self.elo})"
         )
