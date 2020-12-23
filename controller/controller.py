563b91ceef33d382f0d9d5b8d9b2034e95e9e837
Valkea <github-sub@shedge.com>
2020-12-23 17:03:56 +0100

    doc(controller): minor docstring format update
    doc(view): minor docstring format update

--- controller/controller.py
+++ controller/controller.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-''' The purpose of this module is to handle the models
+""" This module handles the controllers
     of this chess tournament manager
-'''
+"""
 
 
-class Model:
-
+class Controller:
     def __init__(self):
         pass
--- view/view.py
+++ view/view.py
@@ -1,12 +1,11 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-''' The purpose of this module is to handle the views
+""" The purpose of this module is to handle the views
     of this chess tournament manager
-'''
+"""
 
 
 class View:
-
     def __init__(self):
         pass
