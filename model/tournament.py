697ca24391fdab1ebc2e12eef8b9ab6c49e9245d
Valkea <github-sub@shedge.com>
2020-12-23 17:02:38 +0100

    feat(model/tournament): create initial methods
    doc(model/tournament): complete the tournament module & class docstring

--- model/tournament.py
+++ model/tournament.py
@@ -1,12 +1,35 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" The purpose of this module is to handle the tournaments
+""" This module handles the tournaments
 """
+
+import datetime
 
 
 class Tournament:
-    """ The purpose of this class is handle the tournament processes """
+    """This class handles the tournaments
+
+    Attributes
+    ----------
+    name : str
+    place : str
+    dates : list(Datetime)
+    num_rounds : int (default is 4)
+    rounds : list(Round)
+    players : list(Player)
+    game_type : str
+    description : str
+
+    Methods
+    -------
+    set_date( str )
+        insert a date using 'DD/MM/YYYY' format 'into the dates list
+    set_dates( list(str) )
+        insert several dates using 'DD/MM/YYYY' format 'into the dates list
+
+
+    """
 
     def __init__(self, name, place, dates, game_type, description, num_rounds=4):
         self.name = name
@@ -17,3 +40,20 @@ class Tournament:
         self.players = []
         self.game_type = game_type
         self.description = description
+
+    def set_date(self, date):
+
+        self.dates.append(datetime.datetime.strptime(date, "%d/%m/%Y"))
+
+    def set_dates(self, dates):
+        for date in dates:
+            self.set_date(date)
+
+    def set_game_type(self, type):
+        if type == "bullet" or type == "blitz" or type == "coup rapide":
+            self.game_type = type
+        else:
+            raise ValueError(
+                "Les contrôles de temps doivent être 'bullet',"
+                "'blitz' ou 'coup rapide'"
+            )
