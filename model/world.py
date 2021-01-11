8a84fd8923fe5022e2a2e26e50d4e1d6ff8d2efe
Valkea <github-sub@shedge.com>
2021-01-11 02:30:14 +0100

    doc: write model/world docstrings

--- model/world.py
+++ model/world.py
@@ -1,8 +1,7 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" This module handles the tournaments
-"""
+""" This module handles the app world """
 
 from model.player import Player
 from model.tournament import Tournament
@@ -10,7 +9,41 @@ import logging
 
 
 class World:
-    """ D """
+    """This ROOT class offer several methods to register and search for tournaments and actors.
+
+    All the apps' data are stored here and accessible from here.
+    All methods are classmethods, so we can access the content using World.method() World.attribute
+
+    Attributes
+    ----------
+
+    actors : dict('UID', Player)
+        The list of all actors
+    tournaments : list(Tournament)
+        The list of all tournaments
+    active_tournament : Tournament
+        The currently active tournament instance
+
+    Public Methods
+    --------------
+    load(tournaments, actors)
+        Replace the class attributes with the provided ones
+    add_tournament(name, place, start_date, end_date, gtype, desc="", rounds=4)
+        Register a new tournament instance
+    set_active_tournament(tournament)
+        Set the currently active tournament instance
+    get_active_tournament()
+        Get the currently active tournament instance or None
+
+    add_actor(actor, tournament=None)
+        Register a new actor to the provided Tournament instance (or the currently active)
+    get_actor(actor_id)
+        Get an actor instance by providing it's UID
+    get_actors(tournament=None)
+        Get all the actors instances of the provided Tournament instance (or the currently active)
+    get_all_actors()
+        Get all the actors instances
+    """
 
     actors = {}
     tournaments = []
@@ -18,6 +51,16 @@ class World:
 
     @classmethod
     def load(cls, tournaments, actors):
+        """Replace the class attributes with the provided ones.
+
+        Parameters
+        ----------
+        tournaments : list(dict)
+            list of tournaments arguments dictionaries
+        actors : list(dict)
+            list of players arguments dictionaries
+        """
+
         cls.actors = {}
         cls.tournaments = []
         cls.active_tournament = None
@@ -37,7 +80,28 @@ class World:
     def add_tournament(
         cls, name, place, start_date, end_date, gtype, desc="", rounds=4
     ):
-        """ D """
+        """Register a new tournament instance.
+
+        Properties
+        ----------
+        name : str
+            The tournament's name
+        place : str
+            The place where the tournament takes place
+        start_date : str
+            The stating date of the tournament
+        end_date : str
+            The ending date of the tournament
+        rounds : int
+            The number of rounds in the tournament (defaut is 4)
+        players : list(int)
+            The registered player instances id of the tournament
+        gtype : str
+            The game method used in the tournament
+        desc : str
+            The tournament director's notes
+        """
+
         tournament = Tournament(
             cls,
             name,
@@ -55,24 +119,35 @@ class World:
 
     @classmethod
     def set_active_tournament(cls, tournament):
-        """ D """
+        """Set the currently active tournament instance.
+
+        Properties
+        ----------
+        tournament : Tournament
+            The tournament instance to set as the currently active one
+        """
+
         cls.active_tournament = tournament
 
     @classmethod
     def get_active_tournament(cls):
-        """ D """
+        """ Get the currently active tournament instance or None. """
+
         return cls.active_tournament
 
     # --- Actors ---
 
     @classmethod
     def add_actor(cls, actor, tournament=None):
-        """Add a player to the current tournament instance
+        """Register a new actor to the provided Tournament instance (or the currently active)
 
         Parameters
         ----------
         actor : Player
             the Player instance to register as a participant of the tournament
+        tournament : Tournament
+            The tournament instance to set as the currently active one
+            If None, the currently active one is used
         """
 
         if type(actor) is not Player:
@@ -88,13 +163,26 @@ class World:
 
     @classmethod
     def get_actor(cls, actor_id):
-        """ D """
+        """Get an actor instance by providing it's UID
+
+        Parameters
+        ----------
+        actor_id : Player
+            the requested Player's instance UID
+        """
 
         return cls.actors.get(actor_id)
 
     @classmethod
     def get_actors(cls, tournament=None):
-        """ D """
+        """Get all the actors instances of the provided Tournament instance (or the currently active)
+
+        Parameters
+        ----------
+        tournament : Tournament
+            The tournament instance used to search the actors
+            If None, the currently active one is used
+        """
 
         if tournament is None:
             tournament = cls.get_active_tournament()
@@ -107,6 +195,6 @@ class World:
 
     @classmethod
     def get_all_actors(cls):
-        """ D """
+        """ Get all the actors instances """
 
         return [v for k, v in cls.actors.items()]
